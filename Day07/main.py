ace9abadc55d1dd791003a04ea27ab44b53d8ccc
Евгения Сергеева <evgeniasergeeva@MacBook-Air-Evgenia.local>
2025-12-13 14:11:24 -0800

    Day07: set и tuple, задачи и мини-проект

---
+++ Day07/main.py
@@ -0,0 +1,34 @@
+# Задача 1 (убрать дубликаты) #
+
+# names = ["anton", "ivan", "anton", "ivan"]
+# unique_name = set(names)
+# print(unique_name)#
+
+# Задача 2(проверка наличия элементов в set)
+
+# users = {"anton", "ivan", "olga"}
+# if "anton" in users:
+#    print("Пользователь найден")
+# else:
+#   print("Пользователь не найден")
+
+# Задача 3 (операция над множествами set)
+# a = {1, 2, 3, 4, 5, 6}                    a & b   # пересечение (общие)
+#                                            a | b   # объединение (все уникальные)
+#                                            a - b   # разница (что есть в a, но нет в b)
+# b = {3, 4, 5, 6, 7, 8}
+
+# common = a & b 
+# print(common).         
+
+# Задача 4 (Создать и использовать tuple)
+# point = (5, 10)
+# print(point)
+# print(point[0])
+# print(point[1])
+
+# Задача 5(Когда использовать tuple, а когда list)
+# days = ("Пн", "Вт", "Ср", "Чт", "Пт")
+#for day in days:
+#   print(day)
+# days[0] = "Понедельник"
---
+++ Day07/users.py
@@ -0,0 +1,5 @@
+logins = ["anton", "ivan", "anton", "olga", "ivan", "petr"]
+unique_logins = set(logins)
+result = tuple(unique_logins)
+
+print(result)
